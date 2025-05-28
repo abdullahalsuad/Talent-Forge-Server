6ef041a7b77e2f9413e5eeab1cb94be0ef1da142
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-28 18:31:49 +0600

    feat: add job retrieval and creation endpoints with error handling

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -9,3 +9,24 @@ export const getAllJobs = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+// Get Job by id
+export const getJobByID = async (req, res) => {
+  try {
+    const job = await JobModel.findById(req.params.id);
+    if (!job) return res.status(404).json({ message: "Job not found" });
+    res.status(200).json(job);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create new job
+export const createJob = async (req, res) => {
+  try {
+    const savedJob = await job.save();
+    res.status(201).json(savedJob);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -4,6 +4,9 @@ import * as jobController from "../controllers/jobController.js";
 const router = express.Router();
 
 // Route to get all jobs
-router.route("/").get(jobController.getAllJobs);
+router.get("/", jobController.getAllJobs);
+
+// Route to create new jobs
+router.post("/", jobController.createJob);
 
 export default router;
