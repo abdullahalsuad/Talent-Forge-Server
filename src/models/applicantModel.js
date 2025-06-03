e6634ad6938e97c4eecc12d49a20ad7861e32b5a
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-06-03 14:59:24 +0600

    feat: update applicant model to include job-related fields

--- src/models/applicantModel.js
+++ src/models/applicantModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const applicantSchema = new mongoose.Schema(
   {
@@ -51,13 +52,25 @@ const applicantSchema = new mongoose.Schema(
       trim: true,
     },
 
-    // Job Creator email
+    // About job
     creatorEmail: {
       type: String,
       trim: true,
     },
+
     jobId: {
       type: String,
+    },
+    jobTitle: {
+      type: String,
+      trim: true,
+    },
+    companyName: {
+      type: String,
+      trim: true,
+    },
+    applicationDeadline: {
+      type: Date,
     },
   },
   {
